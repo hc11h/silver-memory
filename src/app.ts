1dd2397b78f406e43b87a13412d9f9f2727d9454
harsh-krishang <krishangtechnolab@gmail.com>
2025-10-09 12:55:14 +0530

    remove unwanted

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(helmet());
 app.use('/api', apiLimiter);
 
 // Routes
-app.use('/api', apiRouter);
+app.use('/api/v1', apiRouter);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);
