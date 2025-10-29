18b2da37503038d2e591733e9fe15f2b82676fb3
harsh-krishang <krishangtechnolab@gmail.com>
2025-10-29 19:36:49 +0530

    fix path

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import env from '@/config/env';
+import env from '../config/env';
 import { seedRBAC } from '@/seeders/modularPermissionsSeeder';
 
 export const connectDB = async () => {
