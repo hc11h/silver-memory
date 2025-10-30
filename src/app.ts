9262b9f0c47c08fb289ef37a246a1b6644634e91
harsh-krishang <krishangtechnolab@gmail.com>
2025-10-30 10:53:58 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import express from 'express';
+import apiRouter from './routes';
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Test route
+// health route
 app.get('/health', (req, res) => {
-  res.send('🚀 Simple backend is running!');
+  res.status(200).json({
+    status: 'ok',
+    message: 'Server is running',
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.use('/api/v1', apiRouter);
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
 });
 
 export default app;
